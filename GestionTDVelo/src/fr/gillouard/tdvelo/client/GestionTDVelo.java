3d18f46f9421c7374464e92987fe1cb995194121
draoullig <frederic.gillouard@gmail.com>
2013-04-16 22:18:06 +0200

    Ajout de la trame GWT

---
+++ GestionTDVelo/src/fr/gillouard/tdvelo/client/GestionTDVelo.java
@@ -0,0 +1,18 @@
+package fr.gillouard.tdvelo.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GestionTDVelo implements EntryPoint {
+	
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		
+		Window.alert("C'est un debut !");
+	}
+}
